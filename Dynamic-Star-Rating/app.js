0b29b36fb6eca5730f4d708c0c186a985ba4e6ee
VIKRAM P <143996092+VIKRAMpj123@users.noreply.github.com>
2024-01-09 00:28:49 -1000

    Dynamic-Star-Rating-With-HTML-CSS-JavaScript

---
+++ Dynamic-Star-Rating/app.js
@@ -0,0 +1,11 @@
+const stars = document.querySelectorAll(".star");
+
+stars.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    stars.forEach((star, index2) => {
+      index >= index2
+        ? star.classList.add("active")
+        : star.classList.remove("active");
+    });
+  });
+});
